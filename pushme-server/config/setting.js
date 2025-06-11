aaf915c542e7b7f1184ba5eb2fa45025a18230e1
天明 <1036498144@qq.com>
2025-06-11 15:05:19 +0800

    修改pushkey

--- pushme-server/config/setting.js
+++ pushme-server/config/setting.js
@@ -1,5 +1,5 @@
 module.exports = {
-    push_keys: ['mpRIMBveL6ifzkxjvc1u'],
-    user: '7c44d87f494a22712abe0b63b6fa3abc',
-    password: '49ebfcb656f4aadd1c7c13365a6e9ab4'
+    push_keys: [''],
+    user: '',
+    password: ''
 };
