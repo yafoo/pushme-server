dc5ec95bfc997e0b20c2ca6f3aae0a92a5064102
yafu <zhyaphoo@qq.com>
2025-11-28 09:33:49 +0800

    新增证书设置、账号密码设置

--- app/controller/admin.js
+++ app/controller/admin.js
@@ -6,30 +6,25 @@ class Admin extends Base {
         return this.$config.setting ? this.$config.setting.push_keys.join(',') : '';
     }
 
-    _parsePushkey(push_key) {
-        push_key = push_key.replace(/ /g, '').replace(/,/g, "', '");
-        return `'${push_key}'`;
-    }
-
     async _writeSettingFile(data = {}) {
         const setting = {
-            push_key: '',
-            user: '',
-            password: '',
+            push_key: this._getPushkey(),
+            user: this.$config.setting.user,
+            password: this.$config.setting.password,
+            tls: 'none',
             ...data
         };
-        if(setting.push_key !== '') {
-            setting.push_key = this._parsePushkey(data.push_key);
-        } else {
-            setting.push_key = this._getPushkey();
-        }
-        if(!setting.user) {
-            setting.user = this.$config.setting.user;
-        }
-        if(!setting.password) {
-            setting.password = this.$config.setting.password;
-        }
-        const setting_str = `module.exports = {\n    push_keys: [${setting.push_key}],\n    user: '${setting.user}',\n    password: '${setting.password}'\n};`;
+        const _parseKey = (push_key) => {
+            push_key = push_key.replace(/'/g, '\\\'').replace(/ /g, '').replace(/,/g, "', '");
+            return `'${push_key}'`;
+        };
+        setting.push_key = _parseKey(setting.push_key);
+        const setting_str = `module.exports = {
+    push_keys: [${setting.push_key}],
+    user: '${setting.user}',
+    password: '${setting.password}',
+    tls: '${setting.tls}'
+};`;
         const setting_file = path.join(this.$config.app.base_dir, './config/setting.js');
         await require('fs/promises').writeFile(setting_file, setting_str);
         require.cache[setting_file] && delete(require.cache[setting_file]);
--- app/controller/setting.js
+++ app/controller/setting.js
@@ -18,11 +18,28 @@ class Setting extends Admin
 
         if(this.$request.isGet()) {
             this.$assign('push_key', this._getPushkey());
+            this.$assign('tls', this.$config.setting.tls || 'none');
             return this.$fetch();
         }
+
+        const form = this.$request.query('form', '');
+        if(form == 'push_key') {
+            const push_key = this.$request.query('push_key', '');
+            await this._writeSettingFile({push_key});
+        } else if(form == 'tls') {
+            const tls = this.$request.query('tls', 'none');
+            await this._writeSettingFile({tls});
+        } else if(form == 'user') {
+            const user = this.$request.query('user');
+            const password = this.$request.query('password');
+            if(!user || !password) {
+                return this.$error('账号或密码不能为空！');
+            }
+            await this._writeSettingFile({user: this._md5(user), password: this._md5(password)});
+            // 模拟登录
+            this.$cookie.set('user', user);
+        }
         
-        const push_key = this.$request.query('push_key', '');
-        await this._writeSettingFile({push_key});
         this.$success('保存成功！');
     }
 
